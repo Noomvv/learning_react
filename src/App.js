761cec2fe36552d93f46147683665635e252a34a
v_v_moon <vladimir.mun.47@gmail.com>
2025-05-25 21:15:00 +0500

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Image from './components/image';
+import React from 'react';
+import logo1 from './img/IMG_2996.JPG';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      helpText: "Help text!",
+      userData: "",
+    }
+
+    this.inputClick = this.inputClick.bind(this);
+  }
+
+  helpText = "Help text!";
+  render() {
+    return (
+      <div>
+        <Header title="это шапка" />
+        <h1>{this.state.helpText}</h1>
+        <h2>{this.state.userData}</h2>
+        <input placeholder={this.state.helpText}
+          onChange={event => this.setState({userData: event.target.value})}
+          onClick={this.inputClick} onMouseEnter={this.mouseOver} />
+        <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
+        <Image image={logo1} />
+      </div>
+    );
+  }
+
+  inputClick() {
+    this.setState({helpText: "Changed!"})
+    console.log("Clicked");}
+  mouseOver() {console.log("Mouse Over");}
 }
 
 export default App;
