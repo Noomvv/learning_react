26d13760162c3cb85888e81762c8513f65fc3e6f
v_v_moon <vladimir.mun.47@gmail.com>
2025-05-26 02:01:33 +0500

    add user

--- src/App.js
+++ src/App.js
@@ -2,23 +2,57 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Users from './components/Users';
+import AddUser from './components/AddUser';
 import React from 'react';
 
 
 
 class App extends React.Component {
+  constructor(props) {
+        super(props)
+        this.state = {
+            users: [
+                {
+                    id: 1,
+                    firstname: 'Владимир',
+                    lastname: 'Мун',
+                    bio: 'Программист, который любит React',
+                    age: 24,
+                    isHappy: false
+                },
+                {
+                    id: 2,
+                    firstname: 'Софья',
+                    lastname: 'Пыхтина',
+                    bio: 'Пупсик, которая любит React',
+                    age: 19,
+                    isHappy: true
+                }
+            ]
+        }
+        this.addUser = this.addUser.bind(this);
+    }
   
   render() {
     return (
       <div>
         <Header title="Список пользователей" />
         <main>
-          <Users />
-          <aside></aside>
+          <Users users={this.state.users} />
         </main>
+
+        <aside>
+          <AddUser onAdd={this.addUser} />
+        </aside>
       </div>
     );
   }
+
+  addUser(user) {
+    const id = this.state.users.lenght + 1
+    this.setState({ users: [...this.state.users, {id, ...user}] })
+  }
+
 }
 
 export default App;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,13 @@
 import React from 'react';
+import User from './User';
 
 class Users extends React.Component {
-    users = [
-        {
-            id: 1,
-            firstname: 'Bob',
-            lastname: 'Marley',
-            bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            age: 40,
-            isHappy: true
-        },
-        {
-            id: 2,
-            firstname: 'John',
-            lastname: 'Doe',
-            bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            age: 22,
-            isHappy: false
-        }
-    ]
-
   render() {
-    if(this.users.length > 0)
+    if(this.props.users.length > 0)
         return (<div>
-            {this.users.map((el) => (<div className="user" key={el.id}>
-                <h3>{el.firstname} {el.lastname}</h3>
-                <p>{el.bio}</p>
-                <b>{el.isHappy ? 'Счастлив' : 'Не особо'}</b>
-            </div>))}
+            {this.props.users.map((el) => (
+                <User key={el.id} user={el} />
+            ))}
         </div>)
     else
         return (<div className="user">
